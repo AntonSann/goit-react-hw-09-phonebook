5ac7123fb22064b796f513b63c8a421dbcce9fc0
Anton Sannykov <antonsannykov@gmail.com>
2021-05-28 21:42:39 +0300

    fin

--- src/Components/PublicRoute.js
+++ src/Components/PublicRoute.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { authSelectors } from '../Redux/Auth';
 
 /**
  * - Если маршрут ограниченный, и пользователь залогинен, рендерит редирект на /todos
  * - В противном случае рендерит компонент
  */
-const PublicRoute = ({
+
+export default function PublicRoute({
   component: Component,
-  isAuthenticated,
   redirectTo,
   ...routeProps
-}) => (
-  <Route
-    {...routeProps}
-    render={props =>
-      isAuthenticated && routeProps.restricted ? (
-        <Redirect to={redirectTo} />
-      ) : (
-        <Component {...props} />
-      )
-    }
-  />
-);
-
-const mapStateToProps = state => ({
-  isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
-
-export default connect(mapStateToProps)(PublicRoute);
+}) {
+  const isAuthenticated = useSelector(authSelectors.getIsAuthenticated);
+  return (
+    <Route
+      {...routeProps}
+      render={props =>
+        isAuthenticated && routeProps.restricted ? (
+          <Redirect to={redirectTo} />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
