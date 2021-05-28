039183cdde5149b7043934433e34d81b8024931a
Anton Sannykov <antonsannykov@gmail.com>
2021-05-28 20:34:31 +0300

    filter

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import shortid from 'shortid';
 import FilterStyle from "./Filter.module.css";
 import {contactsSelectors, filterContacts} from '../../Redux/Contacts/';
 
-const Filter = ({value, onChangeFilter}) =>{
+export default function Filter() {
+
+  const filterInputId = shortid.generate();
+  const dispatch = useDispatch();
+const value = useSelector(contactsSelectors.getFilter);
+const onChangeFilter = e => dispatch(filterContacts(e.target.value));
+
+  return (
+  <div className={FilterStyle.filter}>
+    <h2>Contacts</h2>
+<label  htmlFor={filterInputId}>Filter by name
+<input
+  type="text"
+  name="filter"
+  required
+  value={value}
+  id={filterInputId}
+  onChange={onChangeFilter}
+/></label></div>
+)
+};
+
+
+/* const Filter = ({value, onChangeFilter}) =>{
     const filterInputId = shortid.generate();
     return(<div className={FilterStyle.filter}>
         <h2>Contacts</h2>
@@ -28,4 +51,4 @@ const mapDispatchToProps = dispatch => ({
   onChangeFilter: e => dispatch(filterContacts(e.target.value)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default connect(mapStateToProps, mapDispatchToProps)(Filter); */
